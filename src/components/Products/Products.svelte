5978fcce436d6e0b5a68c5610ae5a2fabb14170d
Linda-JS <83424817+Linda-JS@users.noreply.github.com>
2021-05-17 19:11:46 +0100

    LJohnson[ADD][UPD]

--- src/components/Products/Products.svelte
+++ src/components/Products/Products.svelte
@@ -1,18 +1,8 @@
 <script>
-  import { onMount, onDestroy } from "svelte";
+  export let title;
   import products from "../../stores/defaultProducts";
   import Product from "./Product.svelte";
-  export let title;
-
-  //// let unsubscribe;
-  // onMount(() => {
-  //   unsubscribe = products.subscribe((value) => {
-  //     localProducts = value;
-  //   });
-  // });
-  // onDestroy(() => {
-  //   unsubscribe();
-  // });
+  import Loading from "../Loading.svelte";
 </script>
 
 <section class="section">
@@ -20,6 +10,8 @@
   <div class="products-center">
     {#each $products as product (product.id)}
       <Product {product} />
+    {:else}
+      <Loading />
     {/each}
   </div>
 </section>
